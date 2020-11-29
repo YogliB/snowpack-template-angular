bf80333bcf1d3a2a16ff645291b510a818df7685
YogliB <yogiboaron@gmail.com>
2020-11-29 23:46:39 +0200

    reload cache

--- plugin.js
+++ plugin.js
@@ -1,41 +1,57 @@
 const execa = require('execa');
 const npmRunPath = require('npm-run-path');
 const cwd = process.cwd();
+const compiler = require('@angular/compiler');
 
 function angularPlugin(_, { args } = {}) {
 	return {
 		name: 'snowpack-plugin-angular',
 		async run({ isDev, log }) {
-			return new Promise((resolve, reject) => {
-				execa.commandSync(
-					`ngcc ${
-						args?.ngccArgs?.trim() ||
-						'--tsconfig ./tsconfig.app.json'
-					}`,
-					{
-						cwd,
-						env: npmRunPath.env(),
-						extendEnv: true,
-						windowsHide: false,
-						stdio: 'inherit',
-					},
-				);
+			const workerPromise = execa.command(
+				`ngc ${
+					args?.ngcArgs?.trim() || '--project ./tsconfig.app.json'
+				} ${isDev ? '--watch' : ''}`,
+				{
+					env: npmRunPath.env(),
+					extendEnv: true,
+					windowsHide: false,
+					cwd,
+				},
+			);
+			const { stdout, stderr } = workerPromise;
 
-				execa.commandSync(
-					`ngc ${
-						args?.ngcArgs?.trim() || '--project ./tsconfig.app.json'
-					}`,
-					{
-						env: npmRunPath.env(),
-						extendEnv: true,
-						windowsHide: false,
-						cwd,
-						stdio: 'inherit',
-					},
-				);
+			function dataListener(chunk) {
+				let stdOutput = chunk.toString();
+				// In --watch mode, handle the "clear" character
+				if (
+					stdOutput.includes('\u001bc') ||
+					stdOutput.includes('\x1Bc')
+				) {
+					log('WORKER_RESET', {});
+					stdOutput = stdOutput
+						.replace(/\x1Bc/, '')
+						.replace(/\u001bc/, '');
+				}
+				log('WORKER_MSG', { level: 'log', msg: stdOutput });
+			}
 
-				resolve();
-			});
+			stdout && stdout.on('data', dataListener);
+			stderr && stderr.on('data', dataListener);
+
+			execa.command(
+				`ngcc ${
+					args?.ngccArgs?.trim() || '--tsconfig ./tsconfig.app.json'
+				}`,
+				{
+					cwd,
+					env: npmRunPath.env(),
+					extendEnv: true,
+					windowsHide: false,
+					stdio: 'inherit',
+				},
+			);
+
+			return workerPromise;
 		},
 	};
 }
