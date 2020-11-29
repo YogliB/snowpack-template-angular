fe9d1b87e5715675e5c4dae04f35cb561f056686
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2020-11-29 15:40:21 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,35 @@
+const execa = require('execa');
+const npmRunPath = require('npm-run-path');
+const cwd = process.cwd();
+
+function angularPlugin(_, {args} = {}) {
+  return {
+    name: 'snowpack-plugin-angular',
+    async run({isDev, log}) {
+      const workerPromise = execa.command(
+		`ngc ${args ? args : '--project ./tsconfig.app.json'} ${isDev ? '--watch' : ''}`,
+        {
+          env: npmRunPath.env(),
+          extendEnv: true,
+          windowsHide: false,
+          cwd,
+        },
+      );
+      const {stdout, stderr} = workerPromise;
+      function dataListener(chunk) {
+        let stdOutput = chunk.toString();
+        // In --watch mode, handle the "clear" character
+        if (stdOutput.includes('\u001bc') || stdOutput.includes('\x1Bc')) {
+          log('WORKER_RESET', {});
+          stdOutput = stdOutput.replace(/\x1Bc/, '').replace(/\u001bc/, '');
+        }
+        log('WORKER_MSG', {level: 'log', msg: stdOutput});
+      }
+      stdout && stdout.on('data', dataListener);
+      stderr && stderr.on('data', dataListener);
+      return workerPromise;
+    },
+  };
+}
+
+module.exports = angularPlugin;
