c4cde2c87e65ea8ca30492618e50e00b73993df5
YogliB <yogiboaron@gmail.com>
2020-11-29 23:35:32 +0200

    wip

--- plugin.js
+++ plugin.js
@@ -6,51 +6,36 @@ function angularPlugin(_, { args } = {}) {
 	return {
 		name: 'snowpack-plugin-angular',
 		async run({ isDev, log }) {
-			const workerPromise = execa.command(
-				`ngc ${
-					args?.ngcArgs?.trim() || '--project ./tsconfig.app.json'
-				} ${isDev ? '--watch' : ''}`,
-				{
-					env: npmRunPath.env(),
-					extendEnv: true,
-					windowsHide: false,
-					cwd,
-				},
-			);
-			const { stdout, stderr } = workerPromise;
+			return new Promise((resolve, reject) => {
+				execa.commandSync(
+					`ngcc ${
+						args?.ngccArgs?.trim() ||
+						'--tsconfig ./tsconfig.app.json'
+					}`,
+					{
+						cwd,
+						env: npmRunPath.env(),
+						extendEnv: true,
+						windowsHide: false,
+						stdio: 'inherit',
+					},
+				);
 
-			function dataListener(chunk) {
-				let stdOutput = chunk.toString();
-				// In --watch mode, handle the "clear" character
-				if (
-					stdOutput.includes('\u001bc') ||
-					stdOutput.includes('\x1Bc')
-				) {
-					log('WORKER_RESET', {});
-					stdOutput = stdOutput
-						.replace(/\x1Bc/, '')
-						.replace(/\u001bc/, '');
-				}
-				log('WORKER_MSG', { level: 'log', msg: stdOutput });
-			}
+				execa.commandSync(
+					`ngc ${
+						args?.ngcArgs?.trim() || '--project ./tsconfig.app.json'
+					}`,
+					{
+						env: npmRunPath.env(),
+						extendEnv: true,
+						windowsHide: false,
+						cwd,
+						stdio: 'inherit',
+					},
+				);
 
-			stdout && stdout.on('data', dataListener);
-			stderr && stderr.on('data', dataListener);
-
-			execa.commandSync(
-				`ngcc ${
-					args?.ngccArgs?.trim() || '--tsconfig ./tsconfig.app.json'
-				}`,
-				{
-					cwd,
-					env: npmRunPath.env(),
-					extendEnv: true,
-					stdio: 'inherit',
-					windowsHide: false,
-				},
-			);
-
-			return workerPromise;
+				resolve();
+			});
 		},
 	};
 }
