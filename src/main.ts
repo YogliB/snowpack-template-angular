113321d478a6aa79c9ee46229c50956b01fcac65
YogliB <yogiboaron@gmail.com>
2020-12-04 22:20:03 +0200

    use snowpack's variables

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
+if ((import.meta as any).env.MODE === 'production') {
 	enableProdMode();
 }
 
