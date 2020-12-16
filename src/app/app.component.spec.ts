6b6b05a51b500c04cf6425ed786904921563a537
YogliB <yogiboaron@gmail.com>
2020-12-16 23:54:02 +0200

    update unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,10 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			imports: [RouterTestingModule],
-			declarations: [AppComponent],
-		}).compileComponents();
-	});
-
-	test('should render title', async () => {
+	test('should render text', async () => {
 		await render(AppComponent, {});
 
-		expect(screen.getByText('Demo app is running!'));
+		expect(screen.getByText('Learn Angular'));
 	});
 });
