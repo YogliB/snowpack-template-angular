8f87c7a10ad0c3c8a3af73e71db82fef825411b9
YogliB <yogiboaron@gmail.com>
2021-01-03 23:43:02 +0200

    prepare for publishing - add descriptive properties - remove browserslist prop - add ability to use `environment` file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { environment as prodEnvironment } from './environment.prod';
 
-export const environment = {
+let environment = {
 	production: false,
 };
+
+if ((import.meta as any).env.MODE === 'production') {
+	environment = prodEnvironment;
+}
+
+export { environment };
 
 /*
  * For easier debugging in development mode, you can import the following file
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if ((import.meta as any).env.MODE === 'production') {
+if (environment.production) {
 	enableProdMode();
 }
 
