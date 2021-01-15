fc5de467bc9262200d8318b03627b57fc53443be
YogliB <yogiboaron@gmail.com>
2021-01-15 20:11:10 +0200

    remove unused code & format file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component, OnDestroy } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
-	count: number = 0;
-	interval = setInterval(() => this.count++, 1000);
-
-	ngOnDestroy() {
-		clearInterval(this.interval);
-	}
-}
+export class AppComponent {}
