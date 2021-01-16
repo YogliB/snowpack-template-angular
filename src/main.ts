5442773b1f39132ec85597db1c03be332cadf80f
YogliB <yogiboaron@gmail.com>
2021-01-16 21:39:54 +0200

    simplify polyfill import

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+
+import './polyfills';
 import 'styles/main.css';
 
 if (environment.production) {
