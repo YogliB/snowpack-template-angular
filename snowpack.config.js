f06857b7e87f2dc731dd80073d5073c37b6593f2
YogliB <yogiboaron@gmail.com>
2021-02-15 21:24:07 +0200

    refactor: remove unnecessary config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,6 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
 	extends: 'snowpack-plugin-angular/snowpack.config.js',
-	mount: { public: '/' },
 	alias: {
 		styles: './public/styles',
 	},
